c7e7036bcf0d120d445c6d3af4251d77fb4489ff
nadeeem0452 <nadeem.clepr@gmail.com>
2019-04-29 11:45:39 +0530

    SignUp Fields Updated

--- users/user.model.js
+++ users/user.model.js
@@ -4,12 +4,11 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     username: { type: String, unique: true, required: true },
     hash: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    age: { type: String, required: true },
-	createdDate: { type: Date, default: Date.now }
+    questionsNecessary: { type: String, required: true },
+    interestedRoommate: { type: String, required: true },
+     createdDate: { type: Date, default: Date.now }
 });
 
 schema.set('toJSON', { virtuals: true });
-
+  
 module.exports = mongoose.model('User', schema);
