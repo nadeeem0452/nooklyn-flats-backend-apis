a5a759c2d7a4b3bf2c4aece35fffdc00e241f1f1
Kunal Sachdeva <>
2019-05-15 11:27:01 +0530

    API> Update NPM to Heroku

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://localhost:/node-mongo-registration-login-api'
+    url: 'mongodb+srv://nooklyn-flats-backend-apis:123321nk@cluster0-6mbxf.mongodb.net/test?retryWrites=true'
 }
