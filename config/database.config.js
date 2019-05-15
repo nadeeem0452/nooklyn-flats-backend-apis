4db60ab0b33eb4b96b4b4512a0aa5e5ace6be41d
Kunal Sachdeva <>
2019-05-15 14:08:12 +0530

    secret updated

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb+srv://nooklyn-flats-backend-apis:123321nk@cluster0-6mbxf.mongodb.net/test?retryWrites=true'
+    url: 'mongodb+srv://nooklyn-flats-backend-apis:123321nk@cluster0-6mbxf.mongodb.net/nooklyn-flats-backend-apis?retryWrites=true'
 }
+
